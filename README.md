53ffcbf6bd0ce1df96cf7b0ed40a795c595bc4bc
Andrew Selbie <aselbie@gmail.com>
2015-07-10 10:36:06 -0700

    Updated instructions

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 ## Developing on the repo
 
-Clone the repo:
+### Install
 ```
-bower install --save ap-file-upload
+> npm install
+> bower install
+> gulp build
+> gulp serve
 ```
 
-- Update ``AUTH0_CLIENT_ID`` in ``gulpfile.coffee``
+### Setup
+
+Sign in using a valid Topcoder account in the domain you're testing.
+
+Modify the parameters of the ``ap-uploader`` directive in ``example/index.jade``
+. The example code in the repo currently is for attaching files to a specific work item. The work microservice requires a valid work request id and requires that you own that work request.
 
 ## API Reference
 http://docs.apworkmicroservice.apiary.io/#reference/workrequestfiles
--- example/scripts/example-app.controller.js
+++ example/scripts/example-app.controller.js
@@ -9,10 +9,8 @@
 
   function ExampleAppController($scope, Uploader, AuthService) {
     var vm = this;
-    vm.uploaderStatus = 'pristine';
 
     vm.authenticated = AuthService.isAuthenticated();
-
     vm.login = function(formData) {
       AuthService.login({
         username: vm.username,
@@ -21,6 +19,34 @@
           vm.authenticated = AuthService.isAuthenticated();
         }
       })
+    }
+
+    vm.uploaderStatus = 'pristine';
+
+    vm.queryUrl = formatUrl({
+      domain: 'http://api.topcoder-dev.com/v3/',
+      path: 'workrequestfiles',
+      filters: {
+        workRequestId: '1436372805000-66d14ff5-ec15-410f-8c51-98e18e75f0fe',
+      }
+    });
+
+    vm.presignerUrl = formatUrl({
+      domain: 'http://api.topcoder-dev.com/v3/',
+      path: 'workrequestfiles/uploadurl',
+      filters: {
+        workRequestId: '1436372805000-66d14ff5-ec15-410f-8c51-98e18e75f0fe',
+        assetType: 'specs'
+      }
+    });
+
+    function formatUrl(params) {
+      var filterArray = [];
+      for (var key in params.filters) {
+        filterArray.push(key + '=' + params.filters[key])
+      }
+      var filterString = '?filter=' + encodeURIComponent(filterArray.join('&'));
+      return params.domain + params.path + filterString;
     }
   }
   
