b550b408c3f13756d7296b7618615ed9ed4ff650
samsep <ssepasi@appirio.com>
2015-11-10 22:17:48 -0800

    update uploader once duplicate files are replaced

--- src/scripts/uploader.service.js
+++ src/scripts/uploader.service.js
@@ -95,7 +95,6 @@
           hasErrors = true;
         }
       });
-      uploader.fileArray = filelistToArray(uploader.files);
       uploader.uploading = uploading;
       uploader.hasErrors = hasErrors;
       uploader.hasFiles = uploader.files.length > 0
@@ -116,6 +115,7 @@
         if (replace) {
           uploader.files[dupePosition].remove().then(function() {
             uploader.files[dupePosition] = newFile;
+            uploader.onUpdate();
           });
         } else {
           deferred.reject('DUPE');
