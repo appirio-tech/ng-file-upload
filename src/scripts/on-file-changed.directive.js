8e44efd07195b5621946c7b5562d9f1633c12296
samsep <ssepasi@appirio.com>
2015-12-15 17:02:01 -0800

    update file names

--- src/scripts/on-file-changed.directive.js
+++ src/scripts/on-file-changed.directive.js
@@ -3,19 +3,19 @@
 
   // Gets around Angular's inability to bind to file input's change event
   // See https://github.com/angular/angular.js/issues/1375
-  angular.module('ap-file-upload').directive('onFileChange', onFileChangeDirective);
+  angular.module('ap-file-upload').directive('onFileChanged', onFileChangedDirective);
 
-  onFileChangeDirective.$inject = [];
+  onFileChangedDirective.$inject = [];
 
-  function onFileChangeDirective() {
+  function onFileChangedDirective() {
     return {
       restrict: 'A',
       scope: {
-        onFileChange: '&'
+        onFileChanged: '&'
       },
       link: function(scope, element, attr, ctrl) {
         element.bind("change", function() {
-          scope.onFileChange({fileList : element[0].files});
+          scope.onFileChanged({fileList : element[0].files});
           this.value = '';
         });
       }
